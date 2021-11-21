b6b6678fa5a596cdb51e5097c8039a68a9ff3302
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-11-21 14:43:41 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    std::vector<int> v{ 1, 2, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9 };
+
+    int countRepit{};
+
+    auto l{ [&countRepit](std::vector<int>& v)
+    {
+        std::unordered_set<int> us;
+
+        for (const auto& elem : v)
+        {
+            if (!us.insert(elem).second) ++countRepit;
+        }
+        
+        auto u_ptr = std::make_unique<std::vector<int>>(us.size());
+
+        std::copy(us.cbegin(), us.cend(), u_ptr.get()->begin());
+
+        return u_ptr;
+    } };
+
+    std::cout << "v: ";
+    std::copy(v.cbegin(), v.cend(), std::ostream_iterator<int>(std::cout, " "));
+    
+    auto u_ptr = l(v);
+
+    std::cout << "\nNumber of repetitions: " << countRepit;
+
+    std::cout << "\nv2: ";
+    std::copy(u_ptr.get()->cbegin(), u_ptr.get()->cend(), std::ostream_iterator<int>(std::cout, " "));
+}
